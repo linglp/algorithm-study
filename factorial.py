2dc2aaeda387b67ca436955ffc42947512da3ff0
linglp <linglp@umich.edu>
2023-11-13 13:57:22 -0500

    compare iterative and recursive

--- factorial.py
+++ factorial.py
@@ -1,3 +1,4 @@
+import timeit
 # implement factorial 
 
 # Example:
@@ -11,6 +12,8 @@
 # when calculating factorial 4, I would need to first calculate factorial 3
 # when calculating factorial 3, I would need to first calculate factorial 2
 
+
+## recursive approach
 def factorial(n):
     if n == 0: 
         return 1
@@ -18,5 +21,27 @@ def factorial(n):
         print(n)
         return n* factorial(n-1)
 
-result = factorial(3)
-print('result', result)
+start = timeit.default_timer()
+factorial(4)
+time_cost = timeit.default_timer() - start
+print('time cost of recursive approach', time_cost)
+
+## iterative approach
+def factorial_iterative(n):
+    cal_result = 1
+    for i in range(2, n):
+        cal_result = cal_result * i
+
+    return cal_result
+
+start_time2 = timeit.default_timer()
+factorial(4)
+time_cost = timeit.default_timer() - start_time2
+print('time cost of recursive approach', time_cost)
+
+
+
+
+
+
+
